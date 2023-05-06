90ff8ea9f4387843dc8db30389726ff1d39e9fc5
Mauricio <mauriciorodriguez90@aragon.unam.mx>
2023-05-06 14:22:21 -0600

    Initial commit

---
+++ src/VentanaGrid.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class VentanaGrid {
+    private Label lblEtiqueta1;
+    private Label lblEtiqueta2;
+    private TextField txtCuadro1;
+    private TextField txtCuadro2;
+    private Button boton1;
+    private FlowLayout layout;
+    private GridLayout layout;
+
+}
